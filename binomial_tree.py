5275f63455ca08bab33efa75f0311ea30aaecf2b
Artem Klyukvin <a.klyukvin@joom.com>
2023-02-06 00:51:55 +0400

    fix

--- binomial_tree.py
+++ binomial_tree.py
@@ -2,17 +2,16 @@ import numpy as np
 
 
 class OptionReplicatingPortfolio:
-    def __init__(self, share_weight, bond_weight, stock_price, is_terminal=False):
+    def __init__(self, share_weight, bond_weight, stock_price):
         self.share_weight = share_weight
         self.bond_weight = bond_weight
         self.stock_price = stock_price
-        self.is_terminal = is_terminal
 
     def get_norm_price(self):
         return self.share_weight + self.bond_weight
 
     def get_price(self):
-        return self.get_norm_price() * self.stock_price
+        return self.share_weight + self.bond_weight * self.stock_price
 
 
 class BinomialTree:
@@ -20,6 +19,9 @@ class BinomialTree:
         def __init__(self, portfolio_map, period_count):
             self._portfolio_map = portfolio_map
             self.period_count = period_count
+
+        def update_portfolio(self, level, index, portfolio):
+            self._portfolio_map[(level, index)] = portfolio
 
         def get_root(self):
             return self.get_portfolio(0, 0)
@@ -29,6 +31,9 @@ class BinomialTree:
 
         def get_children_portfolios(self, level, index):
             return self._portfolio_map[(level + 1, 2 * index)], self._portfolio_map[(level + 1, 2 * index + 1)]
+
+        def has_children_portfolios(self, level, _):
+            return level < self.period_count
 
         def get_period_count(self):
             return self.period_count
@@ -49,10 +54,10 @@ class BinomialTree:
         replicating_portfolios = {}
         stock_price_tree = self._get_stock_price_tree()
 
-        def calculate_replicating_portfolio(payout_up, payout_down, stock_price, is_terminal):
+        def calculate_replicating_portfolio(payout_up, payout_down, stock_price):
             share_weight = (payout_up - payout_down) / (self.up_factor - self.down_factor) / stock_price
             bond_weight = (payout_up / stock_price - share_weight * self.up_factor) / (1 + self.period_discount_rate)
-            return OptionReplicatingPortfolio(share_weight, bond_weight, stock_price, is_terminal)
+            return OptionReplicatingPortfolio(share_weight, bond_weight, stock_price)
 
         def do_calculate(curr_level):
             if curr_level == self.period_count - 1:
@@ -61,7 +66,6 @@ class BinomialTree:
                         self.option.get_payout(stock_price_tree[(curr_level + 1, parent_index * 2)]),
                         self.option.get_payout(stock_price_tree[(curr_level + 1, parent_index * 2 + 1)]),
                         stock_price_tree[(curr_level, parent_index)],
-                        is_terminal=True
                     )
 
                     replicating_portfolios[(curr_level, parent_index)] = portfolio
@@ -79,28 +83,33 @@ class BinomialTree:
                         payout_up,
                         payout_down,
                         stock_price_tree[(curr_level, parent_index)],
-                        is_terminal=False
                     )
 
         do_calculate(0)
 
         return BinomialTree.PortfolioTree(replicating_portfolios, self.period_count)
 
-    # def calculate_replicating_portfolios_american(self):
-    #     portfolio_tree = self.calculate_replicating_portfolios_european()
-    #     up_probability = self._get_risk_neutral_probability()
-    #
-    #     for period_index in range(portfolio_tree.get_period_count() - 1, -1, -1):
-    #         for node_index in range(portfolio_tree.get_node_count_at_period(period_index)):
-    #             portfolio = portfolio_tree.get_portfolio(period_index, node_index)
-    #             if portfolio.is_terminal:
-    #                 continue
-    #
-    #             portfolio_up, portfolio_down = portfolio_tree.get_children_portfolios(period_index, node_index)
-    #
-    #             continuation_price = (
-    #                     portfolio_up.get_price() * up_probability + portfolio_down.get_price() * (1 - up_probability)
-    #             ) / (1 + self.period_discount_rate)
+    def calculate_replicating_portfolios_american(self):
+        portfolio_tree = self.calculate_replicating_portfolios_european()
+        up_probability = self._get_risk_neutral_probability()
+
+        for period_index in range(portfolio_tree.get_period_count() - 1, -1, -1):
+            for node_index in range(portfolio_tree.get_node_count_at_period(period_index)):
+                portfolio = portfolio_tree.get_portfolio(period_index, node_index)
+
+                up_price, down_price = None, None
+                if not portfolio_tree.has_children_portfolios(period_index, node_index):
+                    up_price = self.option.get_payout(portfolio.stock_price * self.up_factor)
+                    down_price = self.option.get_payout(portfolio.stock_price * self.down_factor)
+                else:
+                    portfolio_up, portfolio_down = portfolio_tree.get_children_portfolios(period_index, node_index)
+                    up_price, down_price = portfolio_up.get_price(), portfolio_down.get_price()
+
+                continuation_price = (up_price * up_probability + down_price * (1 - up_probability)) / (1 + self.period_discount_rate)
+                execution_price = self.option.get_payout(portfolio.stock_price)
+
+                # if execution_price > continuation_price:
+
 
     def _get_risk_neutral_probability(self):
         return (1 + self.period_discount_rate - self.down_factor) / (self.up_factor - self.down_factor)
--- test_binomial_tree.py
+++ test_binomial_tree.py
@@ -4,7 +4,7 @@ from option import Option
 
 
 class TestBinomialTree(unittest.TestCase):
-    def test_option_pricing_no_discounting(self):
+    def test_option_pricing_no_discounting_european(self):
         tree = BinomialTree(2, 0.5, 0, 2, 1, Option.long_call_option(0.5))
         portfolio_tree = tree.calculate_replicating_portfolios_european()
 
@@ -23,17 +23,35 @@ class TestBinomialTree(unittest.TestCase):
         self.assertAlmostEqual(-1 / 3, portfolio_root.bond_weight, delta=1e-9)
         self.assertAlmostEqual(1 / 3, portfolio_root.get_price(), delta=1e-9)
 
-    def test_stock_price_scaling(self):
+    # def test_option_pricing_no_discounting_american(self):
+    #     tree = BinomialTree(2, 0.5, 0, 2, 1, Option.long_call_option(0.5))
+    #     portfolio_tree = tree.calculate_replicating_portfolios_american()
+    #
+    #     portfolio_up = portfolio_tree.get_portfolio(1, 0)
+    #     self.assertAlmostEqual(1, portfolio_up.share_weight, delta=1e-9)
+    #     self.assertAlmostEqual(-0.25, portfolio_up.bond_weight, delta=1e-9)
+    #     self.assertAlmostEqual(1.5, portfolio_up.get_price(), delta=1e-9)
+    #
+    #     portfolio_down = portfolio_tree.get_portfolio(1, 1)
+    #     self.assertAlmostEqual(2 / 3, portfolio_down.share_weight, delta=1e-9)
+    #     self.assertAlmostEqual(-1 / 3, portfolio_down.bond_weight, delta=1e-9)
+    #     self.assertAlmostEqual(1 / 6, portfolio_down.get_price(), delta=1e-9)
+    #
+    #     portfolio_root = portfolio_tree.get_portfolio(0, 0)
+    #     self.assertAlmostEqual(2 / 3, portfolio_root.share_weight, delta=1e-9)
+    #     self.assertAlmostEqual(-1 / 3, portfolio_root.bond_weight, delta=1e-9)
+    #     self.assertAlmostEqual(1 / 3, portfolio_root.get_price(), delta=1e-9)
+
+    def test_stock_price_scaling_european(self):
         tree = BinomialTree(2, 0.5, 0, 1, 100, Option.long_call_option(100))
         portfolio_tree = tree.calculate_replicating_portfolios_european()
 
         portfolio = portfolio_tree.get_root()
         self.assertAlmostEqual(2 / 3, portfolio.share_weight, delta=1e-9)
         self.assertAlmostEqual(-1 / 3, portfolio.bond_weight, delta=1e-9)
-        self.assertAlmostEqual(1 / 3, portfolio.get_norm_price(), delta=1e-9)
         self.assertAlmostEqual(1 / 3 * 100, portfolio.get_price(), delta=1e-9)
 
-    def test_discounting_factor(self):
+    def test_discounting_factor_european(self):
         tree = BinomialTree(2, 0.5, 0.5, 1, 100, Option.long_call_option(100))
         portfolio_tree = tree.calculate_replicating_portfolios_european()
 
