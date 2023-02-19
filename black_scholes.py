f86e0667a2e74a973a1f3ab0d8742a0cf693569a
Artem Klyukvin <a.klyukvin@joom.com>
2023-02-19 17:02:32 +0400

    cleanup

--- black_scholes.py
+++ black_scholes.py
@@ -40,34 +40,34 @@ class Option:
 
         return bond_term - stock_term
 
-    def delta_call(self, parameters, step=1e-9):
+    def delta_call(self, parameters, step=1e-3):
         return self._delta(parameters, self.get_call_price, step)
 
-    def delta_put(self, parameters, step=1e-9):
+    def delta_put(self, parameters, step=1e-3):
         return self._delta(parameters, self.get_put_price, step)
 
-    def gamma_call(self, parameters, step=1e-9):
+    def gamma_call(self, parameters, step=1e-3):
         return self._gamma(parameters, self.get_call_price, step)
 
-    def gamma_put(self, parameters, step=1e-9):
+    def gamma_put(self, parameters, step=1e-3):
         return self._gamma(parameters, self.get_put_price, step)
 
-    def theta_call(self, parameters, step=1e-9):
+    def theta_call(self, parameters, step=1e-3):
         return self._theta(parameters, self.get_call_price, step)
 
-    def theta_put(self, parameters, step=1e-9):
+    def theta_put(self, parameters, step=1e-3):
         return self._theta(parameters, self.get_put_price, step)
 
-    def vega_call(self, parameters, step=1e-9):
+    def vega_call(self, parameters, step=1e-3):
         return self._vega(parameters, self.get_call_price, step)
 
-    def vega_put(self, parameters, step=1e-9):
+    def vega_put(self, parameters, step=1e-3):
         return self._vega(parameters, self.get_put_price, step)
 
-    def rho_call(self, parameters, step=1e-9):
+    def rho_call(self, parameters, step=1e-3):
         return self._rho(parameters, self.get_call_price, step)
 
-    def rho_put(self, parameters, step=1e-9):
+    def rho_put(self, parameters, step=1e-3):
         return self._rho(parameters, self.get_put_price, step)
 
     def _delta(self, parameters, price_callback, step):
@@ -81,7 +81,7 @@ class Option:
         center_price = price_callback(parameters.copy())
         right_price = price_callback(parameters.copy(stock_price_offset=step))
 
-        return (right_price - 2 * center_price + left_price) / (4 * step)
+        return (right_price - 2 * center_price + left_price) / (4 * step**2)
 
     def _theta(self, parameters, price_callback, step):
         left_price = price_callback(parameters.copy(maturity_time_offset=-step))
--- test_black_scholes_integration.py
+++ test_black_scholes_integration.py
@@ -7,9 +7,16 @@ pd.set_option('display.max_columns', None)
 
 
 class TestBinomialTree(unittest.TestCase):
-    stock_price = 230
     risk_free_rate = 5e-3
-    volatility = 0.3
+    original_stock_price = 230
+    original_volatility = 0.3
+
+    delta_maturity = -1 / 365
+    # delta_maturity = 0
+    delta_stock_price = 6
+    # delta_stock_price = 2
+    delta_volatility = 100e-4
+    # delta_volatility = 0
 
     original_book = pd.DataFrame({
         "amount": [34_000, 37_000, 20_000],
@@ -19,36 +26,61 @@ class TestBinomialTree(unittest.TestCase):
     })
 
     def test_calculate_portfolio_price(self):
-        original_book = self.fill_book(self.original_book)
+        original_book = self.fill_book(self.original_book, self.original_stock_price, self.original_volatility)
 
         print(original_book)
         print(original_book[["prices", "total_prices", "delta", "gamma", "theta", "vega", "rho"]].to_latex())
         print(original_book.total_prices.sum())
 
     def test_calculate_pnl_linear(self):
-        original_book = self.fill_book(self.original_book)
-
-        dt = -1/365
-        ds = 6
-        dsigma = 100e-4
+        original_book = self.fill_book(self.original_book, self.original_stock_price, self.original_volatility)
 
         price_diff_df = pd.DataFrame({
-            "d_price_maturity": original_book.amount * original_book.theta * dt,
-            "d_price_stock": original_book.amount * original_book.delta * ds,
-            "d_price_volatility": original_book.amount * original_book.vega * dsigma
+            "maturity_term": original_book.amount * original_book.theta * self.delta_maturity,
+            "first_order_stock_price_term": original_book.amount * original_book.delta * self.delta_stock_price,
+            "second_order_stock_price_term": original_book.amount * 0.5 * original_book.gamma * self.delta_stock_price**2,
+            "volatility_term": original_book.amount * original_book.vega * self.delta_volatility
         })
 
-        print(price_diff_df.to_latex())
-        print(price_diff_df.sum())
-        print(price_diff_df.sum().sum())
+        print(price_diff_df.round().to_latex())
+        print(price_diff_df.round().sum())
+        print(price_diff_df.round().sum().sum())
+
+    def test_non_linearity(self):
+        yesterday_book = self.original_book.copy()
+        yesterday_book = self.fill_book(yesterday_book, self.original_stock_price, self.original_volatility)
+
+        today_book = self.original_book.copy()
+        today_book = self.fill_book(
+            today_book,
+            self.original_stock_price + self.delta_stock_price,
+            self.original_volatility
+        )
+
+        linear_diff_1 = yesterday_book.delta * self.delta_stock_price
+        # linear_diff_2 = 0.5 * yesterday_book.gamma * self.delta_stock_price**2
+        linear_diff_2 = 0
+
+        diff_df = pd.DataFrame({
+            "non_linear_diff": today_book.prices - yesterday_book.prices,
+            "linear_diff": linear_diff_1 + linear_diff_2
+        })
+        diff_df["err_percent"] = (diff_df.non_linear_diff - diff_df.linear_diff) / diff_df.non_linear_diff * 100
+
+        print(diff_df)
+        print(diff_df.sum())
 
     def test_calculate_pnl_non_linear(self):
         yesterday_book = self.original_book
-        yesterday_book = self.fill_book(yesterday_book)
+        yesterday_book = self.fill_book(yesterday_book, self.original_stock_price, self.original_volatility)
 
         today_book = self.original_book.copy()
-        today_book.maturity += -1/1_000_000
-        today_book = self.fill_book(today_book)
+        today_book.maturity += self.delta_maturity
+        today_book = self.fill_book(
+            today_book,
+            self.original_stock_price + self.delta_stock_price,
+            self.original_volatility + self.delta_volatility,
+        )
 
         price_diff_df = pd.DataFrame({
             "total_price_0": yesterday_book.total_prices,
@@ -58,7 +90,7 @@ class TestBinomialTree(unittest.TestCase):
         print(price_diff_df.to_latex())
         print(price_diff_df["diff"].sum())
 
-    def fill_book(self, book):
+    def fill_book(self, book, stock_price, volatility):
         bs_option = black_scholes.Option()
 
         bs_prices = []
@@ -69,10 +101,10 @@ class TestBinomialTree(unittest.TestCase):
         rhos = []
         for amount, typ, strike_price, maturity in zip(book.amount, book.type, book.strike_price, book.maturity):
             parameters = black_scholes.Option.OptionParameters(
-                stock_price=self.stock_price,
+                stock_price=stock_price,
                 strike_price=strike_price,
                 risk_free_rate=self.risk_free_rate,
-                volatility=self.volatility,
+                volatility=volatility,
                 maturity_time=maturity
             )
 
